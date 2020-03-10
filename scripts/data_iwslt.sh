d6b5b4f15f9803260a80b5c7271e445117d8012d
Shaform <shaform@gmail.com>
2020-03-10 09:23:41 -0400

    apply BPE

--- scripts/data_iwslt.sh
+++ scripts/data_iwslt.sh
@@ -10,3 +10,23 @@ if [ ! -f ./data/iwslt-2014/test.en ]; then
   # back to root
   cd ..
 fi
+ROOT_DIR=./data/iwslt-2014
+BPE_CODE=$ROOT_DIR/bpe.code
+CAT=$ROOT_DIR/train.cat
+if [ ! -f $BPE_CODE ]; then
+  cat $ROOT_DIR/train.en $ROOT_DIR/train.de > $CAT
+  echo learning bpe for $CAT
+  subword-nmt learn-bpe -s 32000 < $CAT > $BPE_CODE
+fi
+if [ ! -f $ROOT_DIR/test.bpe.en ]; then
+  for lang in en de; do
+    for split in train dev test;
+    do
+      SRC=$ROOT_DIR/$split.$lang
+      TGT=$ROOT_DIR/$split.bpe.$lang
+      echo apply bpe for $SRC
+      subword-nmt apply-bpe -c $BPE_CODE < $SRC > $TGT
+    done
+  done
+  echo
+fi
