a57521e677d935f638a634cb13f294f9d5e1c979
sangkeun00 <sangkeun00@gmail.com>
2020-03-14 03:25:03 -0400

    separate target input and output

--- src/data_set.py
+++ src/data_set.py
@@ -257,17 +257,19 @@ def get_dataloader(dset, batch_size, shuffle=True, num_workers=2):
     pad_id = vocab_src.PAD_ID
 
     def pad_seqs(batch):
-        length = np.array([len(item) for item in batch])
+        length = np.array([len(item) - 1 for item in batch])
         max_length = length.max()
 
         data = torch.zeros((len(batch), max_length), dtype=torch.long)
+        data2 = torch.zeros((len(batch), max_length), dtype=torch.long)
         for i in range(len(batch)):
-            data[i, :len(batch[i])] = batch[i]
+            data[i, :len(batch[i]) - 1] = batch[i][:-1]
+            data2[i, :len(batch[i]) - 1] = batch[i][1:]
         # data = np.array([(item + [pad_id] * (max_length - len(item)))
         #                  for item in batch])
         # data = torch.tensor(data, dtype=torch.long)
         length = torch.tensor(length, dtype=torch.long)
-        return data, length
+        return data, data2, length
 
     def pack_seqs(batch):
         lengths = np.array([torch.sum((b != 0)).item() for b in batch])
@@ -281,8 +283,8 @@ def get_dataloader(dset, batch_size, shuffle=True, num_workers=2):
 
     def my_collate(batch):
         src_seqs, src_lengths = pack_seqs([item[0] for item in batch])
-        tgt_seqs, tgt_lengths = pad_seqs([item[1] for item in batch])
-        return [src_seqs, src_lengths, tgt_seqs, tgt_lengths]
+        tgt_seqs, tgt_seqs2, tgt_lengths = pad_seqs([item[1] for item in batch])
+        return [src_seqs, src_lengths, tgt_seqs, tgt_seqs2, tgt_lengths]
 
     ran_sampler = torch.utils.data.RandomSampler(dset)
     len_sampler = LenMatchBatchSampler(ran_sampler, batch_size=batch_size,
