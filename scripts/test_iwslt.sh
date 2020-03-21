3e5a536213f0e6197bf6e446ff27e18b413a8ffa
Shaform <shaform@gmail.com>
2020-03-20 20:16:00 -0400

    use fairseq-score for evaluation

--- scripts/test_iwslt.sh
+++ scripts/test_iwslt.sh
@@ -34,13 +34,7 @@ mkdir -p outputs
   sed -r 's/(@@ )|(@@ ?$)//g' outputs/test.bpe.en-de > outputs/test.en-de
 # fi
 echo evaluate en-de
-# ./multi-bleu.perl data/iwslt-2014/test.de < outputs/test.en-de
-sacremoses detokenize -l de < outputs/test.en-de > outputs/test.en-de.detok
-sacremoses detokenize -l de < data/iwslt-2014/test.de > data/iwslt-2014/test.de.detok
-sacrebleu data/iwslt-2014/test.de -l en-de < outputs/test.en-de
-sacrebleu data/iwslt-2014/test.de.detok -l en-de < outputs/test.en-de.detok
-sacrebleu --tokenize none data/iwslt-2014/test.de -l en-de < outputs/test.en-de
-sacrebleu --tokenize none data/iwslt-2014/test.de.detok -l en-de < outputs/test.en-de.detok
+fairseq-score -s outputs/test.en-de -r data/iwslt-2014/test.de
 
 # if [ ! -f outputs/test.de-en ]; then
   python -m src.trainer \
@@ -75,10 +69,4 @@ sacrebleu --tokenize none data/iwslt-2014/test.de.detok -l en-de < outputs/test.
   sed -r 's/(@@ )|(@@ ?$)//g' outputs/test.bpe.de-en > outputs/test.de-en
 # fi
 echo evaluate de-en
-# ./multi-bleu.perl data/iwslt-2014/test.en < outputs/test.de-en
-sacremoses detokenize -l en < outputs/test.de-en > outputs/test.de-en.detok
-sacremoses detokenize -l en < data/iwslt-2014/test.en > data/iwslt-2014/test.en.detok
-sacrebleu data/iwslt-2014/test.en -l de-en < outputs/test.de-en
-sacrebleu data/iwslt-2014/test.en.detok -l de-en < outputs/test.de-en.detok
-sacrebleu --tokenize none data/iwslt-2014/test.en -l de-en < outputs/test.de-en
-sacrebleu --tokenize none data/iwslt-2014/test.en.detok -l de-en < outputs/test.de-en.detok
+fairseq-score -s outputs/test.de-en -r data/iwslt-2014/test.en
