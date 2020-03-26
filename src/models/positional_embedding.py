5eccea4e42b58271f0b2ff5e0b620dbd1696b111
Shaform <shaform@gmail.com>
2020-03-26 11:08:03 -0400

    add some comments

--- src/models/positional_embedding.py
+++ src/models/positional_embedding.py
@@ -13,15 +13,18 @@ class PositionalEmbedding(nn.Module):
         self.embed_dim = embed_dim
         half_dim = embed_dim // 2
 
-        scale = math.log(10000.) / (half_dim - 1)
-        emb = torch.exp(torch.arange(half_dim).float() * -scale).unsqueeze(0)
-        pos = torch.arange(max_len).float().unsqueeze(1)
-        pe = emb * pos
+        # scale = 1/(10000^(i/(hd-1)))
+        scale = torch.exp(-torch.arange(half_dim).float() / (half_dim - 1) *
+                          math.log(10000.))
+        # pe = pos/(10000^(i/(hd-1)))
+        pe = torch.arange(max_len).float()[:, None] * scale[None, :]
+        # pe = sin(pos/(10000^(i/(hd-1)))) & cos(pos/(10000^(i/(hd-1))))
         pe = torch.cat([torch.sin(pe), torch.cos(pe)], dim=1)
+
         self.register_buffer('pe', pe)
 
     def forward(self, tokens, shift=0):
-        out = self.pe[shift:shift+tokens.shape[1], :]
+        out = self.pe[shift:shift + tokens.shape[1], :]
         out = out.unsqueeze(0)
 
         return out.detach()
